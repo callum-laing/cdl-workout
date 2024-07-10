3e9e84e8ee200c55922247343b0dced4895a047a
Callum Laing <callum.laing91@hotmail.co.uk>
2024-07-10 23:16:12 +0100

    added option to add multiple different reps to each exercise, added cancel button once exercise input is active, changed complete button, added button to cancel adding a day

--- src/views/WorkoutsView.vue
+++ src/views/WorkoutsView.vue
@@ -9,25 +9,38 @@
         placeholder="Enter day name"
         v-if="!day.nameSet"
       />
+      <button v-if="!day.nameSet" @click="removeDay(index)" class="cancelDayBtn">❌</button>
       <div class="dayWrapper">
         <span v-if="day.nameSet">{{ day.name }}</span>
         <button v-if="day.nameSet" class="addExBtn" @click="toggleExerciseInputs(index)">
-          Add Exercise
+          {{ day.showExerciseInputs ? 'Cancel' : 'Add Exercise' }}
         </button>
         <div v-if="day.showExerciseInputs">
           <div>
             <label for="nameBox">Exercise Name: </label>
             <input class="nameBox inputBox" v-model="day.exercise.name" />
+            <br />
             <label for="setBox">Sets: </label>
             <input class="setBox inputBox" type="number" v-model.number="day.exercise.sets" />
+            <br />
             <label for="repBox">Reps: </label>
-            <input class="repBox inputBox" type="number" v-model.number="day.exercise.reps" />
-            <button class="exBtn" @click="saveExercise(index)">✓</button>
+            <div v-for="(rep, repIndex) in day.exercise.reps" :key="repIndex">
+              <input
+                class="repBox inputBox"
+                type="number"
+                v-model.number="day.exercise.reps[repIndex]"
+              />
+              <button @click="removeRep(index, repIndex)">Remove</button>
+            </div>
+            <button @click="addRep(index)">Add More Reps</button>
+            <br />
+            <button class="exBtn" @click="saveExercise(index)">Complete Setup ✓</button>
           </div>
         </div>
         <ul>
+          <p class="exListTitle">Exercises:</p>
           <li v-for="(exercise, exIndex) in day.exercises" :key="exIndex">
-            {{ exercise.name }}: {{ exercise.sets }} x {{ exercise.reps }}
+            {{ exercise.name }}: {{ exercise.sets }} x {{ exercise.reps.join(', ') }}
           </li>
         </ul>
       </div>
@@ -47,7 +60,7 @@ export default {
       this.days.push({
         name: '',
         nameSet: false,
-        exercise: { name: '', sets: null, reps: null },
+        exercise: { name: '', sets: null, reps: [null] },
         exercises: [],
         showExerciseInputs: false
       })
@@ -64,16 +77,25 @@ export default {
     toggleExerciseInputs(index) {
       this.days[index].showExerciseInputs = !this.days[index].showExerciseInputs
     },
+    addRep(dayIndex) {
+      this.days[dayIndex].exercise.reps.push(null)
+    },
+    removeRep(dayIndex, repIndex) {
+      this.days[dayIndex].exercise.reps.splice(repIndex, 1)
+    },
+    removeDay(index) {
+      this.days.splice(index, 1)
+    },
     saveExercise(index) {
       const exercise = this.days[index].exercise
-      if (exercise.name.trim() && exercise.sets > 0 && exercise.reps > 0) {
+      if (exercise.name.trim() && exercise.sets > 0 && exercise.reps.every((rep) => rep > 0)) {
         exercise.name = this.capitalizeFirstLetter(exercise.name.trim())
         this.days[index].exercises.push({
           name: exercise.name,
           sets: exercise.sets,
-          reps: exercise.reps
+          reps: exercise.reps.slice() // Copy array to avoid reference issues
         })
-        this.days[index].exercise = { name: '', sets: null, reps: null }
+        this.days[index].exercise = { name: '', sets: null, reps: [null] }
         this.days[index].showExerciseInputs = false
       }
     }
@@ -86,6 +108,11 @@ h1 {
   font-size: 2rem;
   margin-top: 20px;
 }
+
+.exListTitle {
+  font-family: 'Caveat', cursive;
+}
+
 .day {
   margin: 20px 0;
 }
@@ -118,14 +145,41 @@ input {
   width: 100px;
 }
 
+.cancelDayBtn {
+  width: 100px;
+  background-color: transparent;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.cancelDayBtn:hover {
+  background-color: darkred;
+}
+
 .addExBtn {
   width: 150px;
 }
+
 .exBtn {
-  background-color: transparent;
+  margin-top: 15px;
+  background-color: green;
   padding: 2px;
   border-radius: 5px;
   border: none;
+}
+
+.exBtn:hover {
+  background-color: white;
+  color: green;
+  cursor: pointer;
+}
+
+button:hover {
+  cursor: pointer;
+  background-color: white;
+  color: black;
 }
 
 .setBox,
