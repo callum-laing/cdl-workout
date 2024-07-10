9423be3e4a4d2bcaab27842c7635805f0880ff88
Callum Laing <callum.laing91@hotmail.co.uk>
2024-07-10 20:09:04 +0100

    added placaeholder to home page

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <h1>Gym Tracker</h1>
+    <h1>CDL Workout</h1>
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/nutrition">Nutrition</RouterLink>
@@ -19,16 +19,32 @@ import { RouterLink, RouterView } from 'vue-router'
   font-size: 1.2em;
 }
 
+header {
+  background-color: rgba(10, 10, 10, 0.2);
+  padding-bottom: 10px;
+}
+
 nav {
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 1.2rem;
+  font-family: 'Caveat', cursive;
 }
 
+nav > a {
+  text-decoration: none;
+  color: white;
+  font-size: 1.4rem;
+}
+
+nav > a:hover {
+  color: rgba(200, 100, 100);
+}
 h1 {
   text-align: center;
   font-size: 5rem;
   padding-top: 25px;
+  font-family: 'Indie Flower', cursive;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,35 @@
 
 <template>
   <main>
-    <h1>This is my homepage</h1>
+    <p>
+      <span>PLEASE READ:</span> Once the Nutrition and Workout pages are built, the home page will
+      showcase a preview of the 2 pages together for users to get an insight into the app before
+      they build it themselves. For now, enjoy a photo of my dog, Fleur!
+    </p>
+    <img src="../assets/doggy.jpg" />
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+  padding-top: 20px;
+}
+
+img {
+  border-radius: 5px;
+}
+
+p {
+  font-size: 2rem;
+}
+
+span {
+  color: yellow;
+  font-weight: bold;
+}
+</style>
