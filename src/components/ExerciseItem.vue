4312873609c51a620dcaf190a5ac20ca475625a6
Callum Laing <callum.laing91@hotmail.co.uk>
2025-10-11 21:28:51 +0100

    added functionality to edit/complete inputs

--- src/components/ExerciseItem.vue
+++ src/components/ExerciseItem.vue
@@ -1,16 +1,28 @@
 <script setup>
+import { ref } from 'vue'
 const model = defineModel('exercises')
+
+const editExercises = ref(true)
+
+const toggleEdit = () => {
+  editExercises.value = !editExercises.value
+}
 </script>
 
 <template>
-  <div>{{ exercises }}</div>
-  <input v-model="exercises.name" placeholder="Exercise name.." type="text" />
-  <input v-model="exercises.sets" placeholder="Sets" type="number" />
-  <input v-model="exercises.reps" placeholder="Reps" type="number" />
+  <div v-if="editExercises">
+    <input v-model="exercises.name" placeholder="Exercise name.." type="text" />
+    <input v-model="exercises.sets" placeholder="Sets" type="number" />
+    <input v-model="exercises.reps" placeholder="Reps" type="number" />
+    <button @click="toggleEdit">Done</button>
+  </div>
 
-  <p>{{ exercises.name }}</p>
-  <p>{{ exercises.sets }}</p>
-  <p>{{ exercises.reps }}</p>
+  <div v-else>
+    <p>{{ exercises.name }}</p>
+    <p>{{ exercises.sets }}</p>
+    <p>{{ exercises.reps }}</p>
+    <button @click="toggleEdit">Edit</button>
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/WorkoutItem.vue
+++ src/components/WorkoutItem.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue'
 import ExerciseItem from './ExerciseItem.vue'
 
 const model = defineModel('workout')
@@ -6,11 +7,24 @@ const model = defineModel('workout')
 const addExercise = () => {
   model.value.exercises.push({ id: crypto.randomUUID(), name: '', sets: 0, reps: 0 })
 }
+
+const editWorkout = ref(true)
+const toggleWorkoutEdit = () => {
+  editWorkout.value = !editWorkout.value
+}
 </script>
 
 <template>
-  <div>{{ workout }}</div>
-  <input v-model="workout.name" placeholder="Name..." type="text" />
+  <div v-if="editWorkout">
+    <input v-model="workout.name" placeholder="Name..." type="text" />
+    <button @click="toggleWorkoutEdit">Done</button>
+  </div>
+  <div v-else>
+    <p>{{ workout.name }}</p>
+    <button @click="toggleWorkoutEdit">Edit</button>
+  </div>
+
+  <!-- <input v-model="workout.name" placeholder="Name..." type="text" /> -->
   <ExerciseItem
     v-for="(exercise, index) in workout.exercises"
     :key="exercise.id"
